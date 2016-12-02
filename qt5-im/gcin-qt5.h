6c224db76b1f0602e45346cf80fdcf8528473400
ChangZhuo Chen (陳昌倬) <czchen@debian.org>
2016-12-03 01:54:45 +0800

    Use QPlatformInputContextFactoryInterface_iid

--- qt5-im/gcin-qt5.h
+++ qt5-im/gcin-qt5.h
@@ -9,7 +9,7 @@ class QGcinPlatformInputContextPlugin : public QPlatformInputContextPlugin
 {
     Q_OBJECT
 public:
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPlatformInputContextFactoryInterface" FILE "gcin.json")
+    Q_PLUGIN_METADATA(IID QPlatformInputContextFactoryInterface_iid FILE "gcin.json")
     QStringList keys() const;
     QGcinPlatformInputContext *create(const QString& system, const QStringList& paramList);
 };
